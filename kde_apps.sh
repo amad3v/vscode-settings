5d002916411b6b10d6a872c98ac8d39544c4ec82
amad3v <55244128+amad3v@users.noreply.github.com>
2021-03-08 19:23:55 +0000

    kde custom pkgs

---
+++ kde_apps.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+pacman -S bluedevil breeze breeze-gtk drkonqi kactivitymanagerd kde-cli-tools kde-gtk-config kdecoration kdeplasma-addons kgamma5 khotkeys kinfocenter kmenuedit kscreen kscreenlocker ksshaskpass ksysguard kwallet-pam kwayland-integration kwayland-server kwin kwrited libkscreen libksysguard plasma-browser-integration plasma-desktop plasma-disks plasma-firewall plasma-integration plasma-nm plasma-pa plasma-sdk plasma-systemmonitor plasma-thunderbolt plasma-vault plasma-workspace plasma-workspace-wallpapers polkit-kde-agent powerdevil sddm-kcm systemsettings xdg-desktop-portal-kde plasma-wayland-session gwenview okular spectacle elisa kmix kdeconnect ktorrent dolphin-plugins dolphin kompare ksystemlog partitionmanager kcron ark filelight kate kcalc kcharselect kdf kdialog keditbookmarks kfind kgpg konsole kwalletmanager markdownpart print-manager sweeper
+
+exit 0
